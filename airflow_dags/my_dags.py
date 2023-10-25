7ced08262900f9afd603701092efea5dda18f192
fatimazahrae-bh <72580954+fatimazahrae-bh@users.noreply.github.com>
2023-10-25 14:25:48 +0100

    Update my_dags.py

--- airflow_dags/my_dags.py
+++ airflow_dags/my_dags.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta 
 
 # Define default_args for your DAG
 default_args = {
